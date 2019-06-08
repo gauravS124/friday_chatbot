2f65221161812ad08b1c5e4f584ac2e2971b787c
gauravS124 <gauravdesu@gmail.com>
2019-06-08 15:35:15 +0530

    requirements updated with gunicorn

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, from ngrok!"
+    return "Hello, from heroku!.your code is working fine"
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
@@ -20,7 +20,7 @@ def sms_reply():
     # Create reply
     response = MessagingResponse()
     # -response.message(reply)
-    
+
     response.message(reply).media("https://cdn.pixabay.com/photo/2019/03/21/15/51/chatbot-4071274__340.jpg")
     return str(response)
 
